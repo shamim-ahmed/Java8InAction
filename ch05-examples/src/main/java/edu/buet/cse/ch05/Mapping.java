5ca9649b987e6199298e15880af6da0f38135892
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-07-25 16:01:10 +1000

    Add some new examples

--- ch05-examples/src/main/java/edu/buet/cse/ch05/Mapping.java
+++ ch05-examples/src/main/java/edu/buet/cse/ch05/Mapping.java
@@ -27,5 +27,17 @@ public class Mapping {
         Arrays.asList("A picture is worth a thousand words", "A barking dog seldom bites");
     proverbList.stream().flatMap(s -> Arrays.stream(s.split(" "))).filter(s -> s.length() > 2)
         .forEach(System.out::println);
+
+    System.out.println();
+
+    // example 4: flatMap
+    List<Integer> numbers1 = Arrays.asList(1, 2, 3, 4, 5);
+    List<Integer> numbers2 = Arrays.asList(6, 7, 8);
+
+    List<int[]> tupleList =
+        numbers1.stream().flatMap(i -> numbers2.stream().map(j -> new int[] {i, j}))
+            .filter(a -> (a[0] + a[1]) % 3 == 0).collect(Collectors.toList());
+
+    tupleList.forEach(a -> System.out.println(a[0] + ", " + a[1]));
   }
 }
---
+++ ch05-examples/src/main/java/edu/buet/cse/ch05/NumericStream.java
@@ -0,0 +1,35 @@
+package edu.buet.cse.ch05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalInt;
+
+import edu.buet.cse.ch05.model.Dish;
+
+public class NumericStream {
+  public static void main(String... args) {
+    // example 1: print a numeric stream
+    List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
+    Arrays.stream(numbers.toArray()).forEach(System.out::println);
+
+    System.out.println();
+
+    // example 2: sum
+    int sumOfCalories = Dish.menu.stream().map(Dish::getCalories).reduce(0, (a, b) -> a + b);
+    System.out.println("sum of calories = " + sumOfCalories);
+
+    System.out.println();
+
+    // example 3: sum
+    sumOfCalories = Dish.menu.stream().mapToInt(Dish::getCalories).sum();
+    System.out.println("sum of calories = " + sumOfCalories);
+
+    System.out.println();
+
+    // example 4: max and OptionalInt
+    OptionalInt maxCalories = Dish.menu.stream().mapToInt(Dish::getCalories).max();
+    maxCalories.ifPresent(n -> System.out.println("maximum calories = " + n));
+    
+    System.out.println();
+  }
+}
