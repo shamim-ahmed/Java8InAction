f7e9cd1e14a2d122110a8f5e4bfdc2504a12c18c
Shamim Ahmed <shamim.buet.99@gmail.com>
2020-08-01 22:15:00 +1000

    Add some new examples

---
+++ ch06-samples/src/main/java/edu/buet/cse/ch06/Grouping.java
@@ -0,0 +1,37 @@
+package edu.buet.cse.ch06;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import edu.buet.cse.ch06.model.Dish;
+
+public class Grouping {
+  public static void main(String... args) {
+    System.out.println("Dishes grouped by type: " + groupDishesByType());
+    System.out.println("Dish names grouped by type: " + groupDishNamesByType());
+    System.out.println("Dish tags grouped by type: " + groupDishTagsByType());
+  }
+
+  private static Map<Dish.Type, List<Dish>> groupDishesByType() {
+    return Dish.menu.stream().collect(Collectors.groupingBy(Dish::getType));
+  }
+
+  private static Map<Dish.Type, List<String>> groupDishNamesByType() {
+    return Dish.menu.stream().collect(Collectors.groupingBy(Dish::getType,
+        Collectors.mapping(Dish::getName, Collectors.toList())));
+  }
+
+  private static Map<Dish.Type, Set<String>> groupDishTagsByType() {
+    Function<Dish, Stream<String>> func = dish -> {
+      List<String> tagList = Dish.dishTags.get(dish.getName());
+      return tagList != null ? tagList.stream() : Stream.empty();
+    };
+
+    return Dish.menu.stream().collect(
+        Collectors.groupingBy(Dish::getType, Collectors.flatMapping(func, Collectors.toSet())));
+  }
+}
